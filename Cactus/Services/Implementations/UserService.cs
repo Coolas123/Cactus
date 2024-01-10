1b288c5e58e24ba77933d12464639d103bf61467
Anton <gluxow.2015@yandex.ru>
2024-01-10 23:26:29 +0300

    Добавление серсива бизнес логики для обекта User
    -Создан интерфейс IUserService для сигнатур методов работы с обектом User
    -Создан класс UserService для реализвации IUserService

---
+++ Cactus/Services/Implementations/UserService.cs
@@ -0,0 +1,88 @@
+﻿using Cactus.Infrastructure;
+using Cactus.Infrastructure.Interfaces;
+using Cactus.Models.Database;
+using Cactus.Models.Responses;
+using Cactus.Models.ViewModels;
+using Cactus.Services.Interfaces;
+using System.Security.Claims;
+
+namespace Cactus.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+		private readonly IUserRepository userRepository;
+
+		public UserService (IUserRepository dbContext)
+		{
+			this.userRepository = dbContext;
+		}
+
+        public async Task<BaseResponse<ClaimsIdentity>> Register(RegisterViewModel model)
+        {
+            User user = await userRepository.GetByEmailAsync(model.Email);
+            if (user != null)
+            {
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Description = "Пользователь с такой почтой зарегистрирован"
+                };
+            }
+            user = new User
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Surname = model.Surname,
+                DateOfBirth = model.DateOfBirth,
+                Gender = model.Gender,
+                Email = model.Email,
+                SystemRoleId = (int)Cactus.Models.Enums.SystemRole.User,
+                HashPassword = HashPassword.Generate(model.Password)
+            };
+            await userRepository.CreateAsync(user);
+            var result = Authenticate(user);
+            return new BaseResponse<ClaimsIdentity>()
+            {
+                Data = result,
+                Description = "Пользователь создан",
+                StatucCode = StatusCodes.Status200OK
+            };
+        }
+
+        public ClaimsIdentity Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType,user.Email),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType,Cactus.Models.Enums.SystemRole.User.ToString()),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType,Cactus.Models.Enums.UserRole.Patron.ToString())
+            };
+            return new ClaimsIdentity(claims, "ApplicationCookie", 
+                ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+        }
+
+        public async Task<BaseResponse<ClaimsIdentity>> Login(LoginViewModel model)
+        {
+            User user = await userRepository.GetByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Description = "Неверный логин"
+                };
+            }
+            if (user.HashPassword != HashPassword.Generate(model.Password))
+            {
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Description = "Неверный пароль"
+                };
+            }
+            var claims = Authenticate(user);
+            return new BaseResponse<ClaimsIdentity>()
+            {
+                Data=claims,
+                StatucCode=StatusCodes.Status200OK
+            };
+        }
+    }
+}
---
+++ Cactus/Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Cactus.Models.Database;
+using Cactus.Models.Responses;
+using Cactus.Models.ViewModels;
+using System.Security.Claims;
+
+namespace Cactus.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<BaseResponse<ClaimsIdentity>> Register(RegisterViewModel model);
+        ClaimsIdentity Authenticate(User user);
+
+        Task<BaseResponse<ClaimsIdentity>> Login(LoginViewModel model);
+    }
+}
